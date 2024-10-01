a9ce377ccc6485557a4328b6fb34dbba371a46b9
MQuang <65891056+quang2652001@users.noreply.github.com>
2024-10-01 19:48:59 +0700

    binance price deserialize util

--- src/main/java/com/example/demo/util/BinancePriceDeserializer.java
+++ src/main/java/com/example/demo/util/BinancePriceDeserializer.java
@@ -1,2 +1,27 @@
-package com.example.demo.util;public class BinancePriceDeserializer {
+package com.example.demo.util;
+
+import com.example.demo.model.price.BinancePrice;
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+
+public class BinancePriceDeserializer implements JsonDeserializer<BinancePrice> {
+    @Override
+    public BinancePrice deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+        BinancePrice binancePrice = new BinancePrice();
+        binancePrice.setSymbol(jsonObject.get("symbol").getAsString());
+
+        try {
+            binancePrice.setBidPrice(new BigDecimal(jsonObject.get("bidPrice").getAsString()));
+            binancePrice.setBidQty(new BigDecimal(jsonObject.get("bidQty").getAsString()));
+            binancePrice.setAskPrice(new BigDecimal(jsonObject.get("askPrice").getAsString()));
+            binancePrice.setAskQty(new BigDecimal(jsonObject.get("askQty").getAsString()));
+        } catch (NumberFormatException e) {
+            throw new JsonParseException("Error parsing numeric fields", e);
+        }
+
+        return binancePrice;
+    }
 }
