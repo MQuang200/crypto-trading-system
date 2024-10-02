36f7ed944a17cbb73f432ce6fd9f658f7e9b3e87
MQuang <65891056+quang2652001@users.noreply.github.com>
2024-10-02 13:49:38 +0700

    config jdbc

---
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class AppConfig {
+
+    public JdbcTemplate jdbcTemplate(@Autowired DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
